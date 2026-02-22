a914b23b09d12a345c84fcf95b2b4bea914f4489
sakshighogale-153 <sakshighogale153@gmail.com>
2026-02-22 12:14:40 +0530

    Create task1_even_odd.py

---
+++ task1_even_odd.py
@@ -0,0 +1,10 @@
+# Task 1: Check if a Number is Even or Odd
+
+# Take integer input from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
